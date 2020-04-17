d47a26b094457c9f1c6026bfc326f74e5ecf8a7d
TimmKroe <kroeger.timm@gmx.de>
2020-04-17 11:36:20 +0200

    implemented basic charts with dummy data
    Signed-off-by: TimmKroe <kroeger.timm@gmx.de>

--- src/Pages/SingleServer.vue
+++ src/Pages/SingleServer.vue
@@ -8,25 +8,116 @@
                         <small>{{this.singleServer(this.serverName).status}}</small>
                     </h1>
                     <h2 class="subtitle">
-                        {{ this.singleServer(this.serverName).server_type.name.toUpperCase() + ', ' +  this.singleServer(this.serverName).image.name.replace("-", " ")}}
+                        {{ this.singleServer(this.serverName).server_type.name.toUpperCase() + ', ' +
+                        this.singleServer(this.serverName).image.name.replace("-", " ")}}
                     </h2>
                 </div>
             </div>
         </section>
-        <h3 class="is-size-3"></h3>
+
+        <section class="card chart">
+            <div class="card-header">
+                <p class="card-header-title">
+                    CPU Usage
+                </p>
+                <a class="card-header-icon" aria-label="more options">
+                  <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </a>
+            </div>
+            <div class="card-content">
+                <Chart :chartData="this.cpuData"/>
+            </div>
+        </section>
+
+        <section class="card chart">
+            <div class="card-header">
+                <p class="card-header-title">
+                    Disk Usage
+                </p>
+                <a class="card-header-icon" aria-label="more options">
+                  <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </a>
+            </div>
+            <div class="card-content">
+                <Chart :chartData="this.diskData" />
+            </div>
+        </section>
+
+        <section class="card chart">
+            <div class="card-header">
+                <p class="card-header-title">
+                    Network Usage
+                </p>
+                <a class="card-header-icon" aria-label="more options">
+                  <span class="icon">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </a>
+            </div>
+            <div class="card-content">
+                <Chart :chartData="this.netData"/>
+            </div>
+        </section>
 
     </div>
 </template>
 
 <script>
-     import {mapState, mapGetters} from "vuex";
+    import {mapState, mapGetters} from "vuex";
+    import Chart from "@/components/Chart";
 
     export default {
         name: "SingleServer",
-        components: {},
+        components: {Chart},
         data() {
             return {
                 serverName: this.$route.path.toString().replace("/servers/", ""),
+                visibleCPU: true,
+                cpuData: {
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                    datasets: [
+                        {
+                            label: 'Data One',
+                            backgroundColor: '#f87979',
+                            data: [40, 39, 10, 40, 39, 80, 100]
+                        }
+                    ]
+                },
+                diskData: {
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                    datasets: [
+                        {
+                            label: 'Data One',
+                            borderColor: '#000000',
+                            data: [40, 39, 10, 40, 39, 80, 100]
+                        },
+                        {
+                            label: 'Data Two',
+                            borderColor: '#939393',
+                            data: [45, 3, 100, 30, 50, 60, 100]
+                        },
+                    ]
+                },
+                netData: {
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                    datasets: [
+                        {
+                            label: 'Data One',
+                            borderColor: '#ffc900',
+                            data: [40, 39, 10, 40, 39, 80, 100]
+                        },
+                        {
+                            label: 'Data Two',
+                            borderColor: '#52ff00',
+                            data: [45, 3, 100, 30, 50, 60, 100]
+                        },
+                    ]
+                },
+
             }
         },
         computed: {
@@ -40,10 +131,12 @@
         },
         created() {
             this.$store.dispatch('servers/getAllServers')
-        }
+        },
     }
 </script>
 
 <style scoped>
-
+    .chart {
+        margin-top: 25px;
+    }
 </style>
---
+++ src/components/Chart.vue
@@ -0,0 +1,50 @@
+<script>
+    import {Line} from 'vue-chartjs';
+
+    export default {
+        extends: Line,
+        name: "CPUChart",
+        props: {
+            chartData: {
+                type: Object,
+                default: null
+            },
+        },
+        data() {
+            return {
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            },
+                            gridLines: {
+                                display: true
+                            }
+                        }],
+                        xAxes: [ {
+                            gridLines: {
+                                display: false
+                            }
+                        }]
+                    },
+                    legend: {
+                        display: false
+                    },
+                    responsive: true,
+                    maintainAspectRatio: false,
+                }
+            }
+        },
+        async mounted() {
+            console.log(this.chartData)
+            this.renderChart(this.chartData, this.options)
+        }
+    }
+</script>
+
+<style scoped>
+    canvas {
+        height: 200px;
+    }
+</style>
