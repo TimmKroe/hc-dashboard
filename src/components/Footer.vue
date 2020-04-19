8e7f85a582219bac71f1aae67d136003d663871f
TimmKroe <kroeger.timm@gmx.de>
2020-04-19 10:46:46 +0200

    fixed link styling in Footer.vue
    Signed-off-by: TimmKroe <kroeger.timm@gmx.de>

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,11 +3,11 @@
         <div class="flex flex-row">
             <p class="w-full text-left text-gray-500">
                 &copy; 2020
-                <a class="hover:text-blue-700"
+                <a class="hover:text-blue-900"
                    href="https://timmkroe.de/hc-dashboard/">hc-dashboard</a> by <a
-                    class="hover:text-blue-700" href="https://timmkroe.de">TimmKroe</a></p>
+                    class="hover:text-blue-900" href="https://timmkroe.de">TimmKroe</a></p>
             <p class="w-full text-gray-500">2020.2 dev</p>
-            <p class="w-full text-right text-gray-500"><a class="hover:text-blue-700"
+            <p class="w-full text-right text-gray-500"><a class="hover:text-blue-900"
                                                           href="https://github.com/TimmKroe/hc-dashboard/blob/master/LICENSE">GPL-3.0</a>
             </p>
         </div>
